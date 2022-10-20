dd1e274e74d7b8934f730f28f6383924afd6363d
elo <elo>
2022-10-20 16:48:39 +0200

    add new model Osoba

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,3 +10,22 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Osoba(models.Model):
+    MONTHS = (
+        (1, 'Styczeń'),
+        (2, 'Luty'),
+        (3, 'Marzec'),
+        (4, 'Kwiecień'),
+        (5, 'Maj'),
+        (6, 'Czerwiec'),
+        (7, 'Lipiec'),
+        (8, 'Sierpień'),
+        (9, 'Wrzesień'),
+        (10, 'Październik'),
+        (11, 'Listopad'),
+        (12, 'Grudzień'),
+    )
+    imie = models.CharField(max_length = 30) 
+    nazwisko = models.CharField(max_length = 30)
+    miesiac_urodzenia = models.IntegerField( default = 0, choices = MONTHS)
