35e2f99b7dc735470a12fbc8659d54b0251a0b1d
Bklzn <adrian_bon@wp.pl>
2022-11-12 18:00:21 +0100

    lab 5, task 7 - fix

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -76,31 +76,6 @@ class OsobaNameFit(APIView):
         osoba.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def osoba_namefit(request, input):
-    try:
-        osoba = Osoba.objects.filter(imie__icontains = input)
-        if(len(osoba) < 1):
-            osoba = Osoba.objects.filter(nazwisko__icontains = input)
-    except osoba.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = OsobaSerializer(osoba, many = True)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = OsobaSerializer(osoba, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        osoba.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 @api_view(['GET'])
 def druzyna_list(request):
     if request.method == 'GET':
